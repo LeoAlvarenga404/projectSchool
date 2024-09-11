7b120d05d11257cd1475a81e47d3a778d1ec75d7
leonardo.alvarenga <leonardo.alvarenga@anchieta.br>
2024-09-11 18:43:02 -0300

    criado rota para fazer login

--- src/controllers/alunoController.ts
+++ src/controllers/alunoController.ts
@@ -17,17 +17,18 @@ export const selectAluno = async (cod_aluno: number): Promise<any> => {
   return result.recordset
 }
 
-export const insertAluno = async (nome: string, cod_curso: number, email: string, data_nascimento: string, situacao_aluno: string): Promise<void> => {
+export const insertAluno = async (nome: string, senha: string, cod_curso: number, email: string,  data_nascimento: string, situacao_aluno: string): Promise<void> => {
   const pool = await conexao()
   await pool.request()
   .input('NOME', nome)
+  .input('SENHA', senha)
   .input('COD_CURSO', cod_curso)
   .input('EMAIL', email)
   .input('DATA_NASCIMENTO',data_nascimento)
   .input('SITUACAO_ALUNO',situacao_aluno)
   .query(`
-      INSERT INTO Aluno (nome, cod_curso, email, data_nascimento, situacao_aluno) 
-      VALUES (@NOME, @COD_CURSO, @EMAIL, @DATA_NASCIMENTO, @SITUACAO_ALUNO);
+      INSERT INTO Aluno (nome, senha, cod_curso, email, data_nascimento, situacao_aluno) 
+      VALUES (@NOME, @SENHA, @COD_CURSO, @EMAIL, @DATA_NASCIMENTO, @SITUACAO_ALUNO);
     `)
 }
 
@@ -41,23 +42,38 @@ export const deleteAluno = async (cod_aluno: number): Promise<void> => {
   
 };
 
-export const updateAluno = async (cod_aluno: number, nome?: string, cod_curso?: number, email?: string, data_nascimento?: string, situacao_aluno?: string): Promise<void> => {
+export const updateAluno = async (cod_aluno: number, nome?: string, senha?: string, cod_curso?: number, email?: string, data_nascimento?: string, situacao_aluno?: string): Promise<void> => {
   const pool = await conexao();
   await pool.request()
   .input('COD_ALUNO', cod_aluno)
-    .input('NOME', nome)
-    .input('COD_CURSO', cod_curso)
-    .input('EMAIL', email)
-    .input('DATA_NASCIMENTO', data_nascimento)
-    .input('SITUACAO_ALUNO', situacao_aluno)
-    .query(`
-      UPDATE Aluno
-      SET 
-        nome = COALESCE(@NOME, nome),
-        cod_curso = COALESCE(@COD_CURSO, cod_curso),
-        email = COALESCE(@EMAIL, email),
-        data_nascimento = COALESCE(@DATA_NASCIMENTO, data_nascimento),
-        situacao_aluno = COALESCE(@SITUACAO_ALUNO, situacao_aluno)
-      WHERE cod_aluno = @COD_ALUNO
-    `);
+  .input('NOME', nome)
+  .input('SENHA', senha)
+  .input('COD_CURSO', cod_curso)
+  .input('EMAIL', email)
+  .input('DATA_NASCIMENTO', data_nascimento)
+  .input('SITUACAO_ALUNO', situacao_aluno)
+  .query(`
+    UPDATE Aluno
+    SET 
+      nome = COALESCE(@NOME, nome),
+      senha = COALESCE(@SENHA, senha),
+      cod_curso = COALESCE(@COD_CURSO, cod_curso),
+      email = COALESCE(@EMAIL, email),
+      data_nascimento = COALESCE(@DATA_NASCIMENTO, data_nascimento),
+      situacao_aluno = COALESCE(@SITUACAO_ALUNO, situacao_aluno)
+    WHERE cod_aluno = @COD_ALUNO
+  `);
+}
+
+export const loginUsuario = async ( email: string, senha: string): Promise<any> => {
+  const pool = await conexao()
+  const result = await pool.request()
+  .input('EMAIL', email)
+  .input('SENHA', senha)
+  .query(`
+    SELECT * FROM Aluno
+    WHERE email = @EMAIL
+  `);
+
+  return result.recordset
 }
--- src/routes/alunoRoutes.ts
+++ src/routes/alunoRoutes.ts
@@ -1,14 +1,19 @@
 import { FastifyInstance } from 'fastify';
-import { selectTodosAlunos, insertAluno, deleteAluno, updateAluno, selectAluno } from '../controllers/alunoController';
+import { selectTodosAlunos, insertAluno, deleteAluno, updateAluno, selectAluno, loginUsuario } from '../controllers/alunoController';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken'
+
 import { z } from 'zod';
 
 const alunoSchema = z.object({
   nome: z.string().min(1),
+  senha: z.string().min(4, 'A senha precisa ter mais de 4 caracters'),
   cod_curso: z.number().int().min(1),
   email: z.string().email('Email inválido'),
   data_nascimento: z.string().refine(val => Date.parse(val)),
   situacao_aluno: z.string().min(1)
 });
+
 
 export const alunoRoutes = async (app: FastifyInstance) => {
   app.get('/alunos', async (req, reply) => {
@@ -17,7 +22,7 @@ export const alunoRoutes = async (app: FastifyInstance) => {
   });
 
   app.get('/alunos/:id', async (req, reply) => {
-    const params = req.params as unknown as { id: string };
+    const params = req.params as { id: string };
     const cod_aluno = parseInt(params.id, 10);
 
     if (!cod_aluno) {
@@ -29,17 +34,21 @@ export const alunoRoutes = async (app: FastifyInstance) => {
   })
 
   app.post('/alunos', async (req, reply) => {
-    const parsedBody = alunoSchema.parse(req.body);
-
+    const pasrseBody = alunoSchema.parse(req.body);
+    
+    const senhaCriptografada = await bcrypt.hash(pasrseBody.senha, 10)
+    
+    
     await insertAluno(
-      parsedBody.nome,
-      parsedBody.cod_curso,
-      parsedBody.email,
-      parsedBody.data_nascimento,
-      parsedBody.situacao_aluno
+      pasrseBody.nome,
+      pasrseBody.senha = senhaCriptografada,
+      pasrseBody.cod_curso,
+      pasrseBody.email,
+      pasrseBody.data_nascimento,
+      pasrseBody.situacao_aluno
     );
 
-    reply.status(201).send({ message: 'Aluno criado com sucesso'})
+    reply.status(201).send({ message: "Aluno criado com sucesso!" })
   });
 
   app.delete('/alunos/:id', async (req, reply) => {
@@ -57,32 +66,59 @@ export const alunoRoutes = async (app: FastifyInstance) => {
 
   app.put('/alunos/:id', async (request, reply) => {
     try {
-      const params = request.params as unknown as { id: string };
+      const params = request.params as { id: string };
       const id = parseInt(params.id, 10);
-      if (isNaN(id)) {
+
+      if (!id) {
         return reply.status(400).send({ error: 'ID inválido' });
       }
 
       const parsedBody = alunoSchema.parse(request.body);
+ 
 
       await updateAluno(
         id,
         parsedBody.nome,
+        parsedBody.senha,
         parsedBody.cod_curso,
         parsedBody.email,
         parsedBody.data_nascimento,
         parsedBody.situacao_aluno
       );
 
-      reply.status(200).send({ message: 'Aluno atualizado com sucesso' });
+      reply.status(200).send({ message: "Aluno atualizado com sucesso", parsedBody });
     } catch (error) {
-      if (error instanceof z.ZodError) {
-        reply.status(400).send({ error: error.errors });
-      } else {
-        reply.status(500).send({ error: 'Internal Server Error' });
-      }
+      console.error(error)
     }
   });
 
-};
+
+  app.post('/login', async (request, reply) => {
+    try {
+      const { email, senha } = request.body as { email: string, senha: string }
+
+      if (!email || !senha) {
+        return reply.status(400).send({ error: "Email e senha são obrigatórios"})
+      }
+
+      const aluno = await loginUsuario(
+        email,
+        senha
+      )
+
+      const senhaValida = await bcrypt.compare(senha, aluno.senha)
+      if (!aluno && !senhaValida) {
+        return reply.status(401).send({ error: "Email ou senha inválidos"})
+      }
+      
+      const token = jwt.sign({ cod_aluno: aluno.cod_aluno, email: aluno.email }, process.env.JWT_SECRET || '', {expiresIn: '1h'})
+
+      return reply.send({ aluno, token })
+
+    } catch(error) {
+      console.error(error)
+    }
+  })
+
+}
 
